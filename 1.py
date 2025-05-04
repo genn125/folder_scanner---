3ea6e68ea696df404c89df3e288d39ad746bbe26
genn125 <genn125@gmail.com>
2025-05-05 01:00:14 +0300

    .

--- 1.py
+++ 1.py
@@ -1,27 +1,37 @@
 import os
 from datetime import datetime
 import sys
-import subprocess
-import json
-from pathlib import Path
+#import subprocess
+#import json
+#from pathlib import Path
 
 AUDIO_EXTENSIONS = {'.mp3', '.flac', '.wav', '.ogg', '.m4a', '.aac', '.wma'}
 # 1 Проверяет наличие музыкальных файлов в папках
 def has_music_files(folder_path):
-    """Проверить наличие музыкальных файлов в папках"""
     for root, _, files in os.walk(folder_path):
         for file in files:
             if os.path.splitext(file)[1].lower() in AUDIO_EXTENSIONS:
                 return True
     return False
-# 2 Рекурсивно сканирует папку и сохраняет структуру с метаданными
-def scan_directory(directory, output_file):
+# 2 Сканирует ТОЛЬКО папки с музыкой
+def scan_music_folders(folder_path):
+    #music_folders = str()
+    with os.scandir(folder_path) as entries:
+        for entry in entries:
+            if has_music_files(entry):
+                music_folders=f'{entry.path}'# Тут передаем строку в отличие от сканера 1 уровня
+    return music_folders
+
+# 3 Рекурсивно сканирует и сохраняет структуру с метаданными в файл
+def scan_directory(music_folders, output_file):
     try:
         with open(output_file, "w", encoding="utf-8") as f:
-            for root, dirs, files in os.walk(directory):
-                level = root.replace(directory, "").count(os.sep)
+            f.write(f"        Мои группы. \nДата создания списка: ({datetime.now().strftime('%H:%M %d-%B-%y')})\n\n")
+            for root, dirs, files in os.walk(music_folders):
+                level = root.replace(music_folders, "").count(os.sep)
                 indent = "│   " * level
                 # Папка
+
                 f.write(f"{indent}│   ├─📁 {os.path.basename(root)}/\n")
                 # Файлы
                 for file in files:
@@ -38,32 +48,27 @@ def scan_directory(directory, output_file):
         print(f"Ошибка сканирования: {e}", file=sys.stderr)
         return False
 
+
+
 def main():
-    print("🔍 Глубокий сканер папок с файлами")
-    target_dir = r'\\bananovoeVeslo\2Музыка\1 РУССКАЯ' #"/storage/emulated/0/Music"#'C:\Users\genn1\Downloads'#
-
-
-    #3. Проверка
-    if not os.path.isdir(target_dir):
-        print(f"❌ Ошибка: Папка '{target_dir}' не найдена!", file=sys.stderr)
-        sys.exit(1)
-
-    # 4. Сканирование
+    print("🔍 ...---===Глубокий сканер папок с файлами===---...")
+    folder_path = r'C:\Users\genn1\Downloads'#'\\bananovoeVeslo\2Музыка\1 РУССКАЯ' #"/storage/emulated/0/Music"#
     output_file = f"Сканер_папок_с_файлами ({datetime.now().strftime('%H_%M  %d-%B-%y')}).txt"
-    print(f"Сканирую '{target_dir}'...")
-    if scan_directory(target_dir, output_file):
-        print(f"✅ Результат сохранён в '{output_file}'")
+    print(f"\nСканирую '{folder_path}'...")
+
+
+    """Сканирует только папки с музыкой"""
+    music_folders = scan_music_folders(folder_path)
+
+    if scan_directory(music_folders, output_file):
+        print(f"✅ Результат сохранён в файл\n   '{output_file}'")
     else:
         print("❌ Сканирование завершено с ошибками", file=sys.stderr)
 
-
-    print(f"\nОтсортированная структура сохранена в файле\n {output_file}")
     print("\nПример содержимого:\n")
     with open(output_file, 'r', encoding='utf-8') as f:
-        n = 250  # печатаем первые n символов из созданного файла
+        n = 5550  # печатаем первые n символов из созданного файла
         print(f.read(n))
-
-
 
 if __name__ == "__main__":
     print("\nСКРИПТ ЗАПУСКАЕТСЯ НАПРЯМУЮ\n")
--- folder_scanner_level1.py
+++ folder_scanner_level1.py
@@ -1,8 +1,8 @@
 import os
 from datetime import datetime
+import sys
 # import subprocess
 # import json
-# import sys
 # from pathlib import Path
 
 AUDIO_EXTENSIONS = {'.mp3', '.flac', '.wav', '.ogg', '.m4a', '.aac', '.wma'}
@@ -14,44 +14,50 @@ def has_music_files(folder_path):
                 return True
     return False
 # 2 Сканирует ТОЛЬКО папки с музыкой
-
 def scan_music_folders(folder_path):
     music_folders = set()
     with os.scandir(folder_path) as entries:
         for entry in entries:
             if has_music_files(entry):
-                music_folders.add(entry)
-
+                music_folders.add(entry) # Тут передаем множество в отличие от глубокого сканера
     return music_folders
-# 3 Сортирует и сохраняет папки в файл
+
+# 3 Сортирует и сохраняет структуру в файл
 def sorted_save_folders(music_folders,output_file):
-    sorted_folders = sorted(music_folders, key=lambda x: x.name)
-    #sorted_folders = sorted(music_folders, key=lambda x: x.stat().st_mtime)  # по времени изменения
-    with open(output_file, "w", encoding="utf-8") as fa:
-        fa.write(f"Мои группы дата создания списка: ({datetime.now().strftime('%H:%M %d-%B-%y')})")
-        for folder in sorted_folders:
-            fa.write(f"\n{folder.name}")
+    try:
+        sorted_folders = sorted(music_folders, key=lambda x: x.name)
+        #sorted_folders = sorted(music_folders, key=lambda x: x.stat().st_mtime)  # по времени изменения
+        with open(output_file, "w", encoding="utf-8") as f:
+            f.write(f"        Мои группы. \nДата создания списка: ({datetime.now().strftime('%H:%M %d-%B-%y')})\n")
+            for folder in sorted_folders:
+                f.write(f"\n ---> {folder.name}")
+        return  True
+    except Exception as e:
+        print(f"Ошибка сканирования: {e}", file=sys.stderr)
+        return False
+
 
 def main():
-    print("\n===== Сканирование папок 1 уровня =====")
-    folder_path = r"C:\Users\genn1\Downloads"#"/storage/emulated/0/Music"#
+    print("🔍 ===== Сканирование папок 1 уровня =====")
+    folder_path = r"C:\Users\genn1\Downloads"#'\\bananovoeVeslo\2Музыка\1 РУССКАЯ'#"/storage/emulated/0/Music"#
     output_file = f"Сканер_папок_1_уровня ({datetime.now().strftime('%H_%M  %d-%B-%y')}).txt"
+    print(f"\nСканирую '{folder_path}'...")
 
-    """Проверяем на наличие музыкальных файлов"""
-    #has_music_files(folder_path)
     """Сканирует только папки с музыкой"""
     music_folders = scan_music_folders(folder_path)
-    """Сортирует и пишет в файл папки с музыкой"""
-    sorted_save_folders(music_folders, output_file)
 
-    print(f"\nОтсортированная структура сохранена в файле\n {output_file}")
+    if sorted_save_folders(music_folders,output_file):
+        print(f"✅ Результат сохранён в файл\n   '{output_file}'")
+    else:
+        print("❌ Сканирование завершено с ошибками", file=sys.stderr)
+
     print(f"\nПример содержимого:\n")
     with open(output_file, 'r', encoding='utf-8') as f:
         n = 200 #печатаем первые n символов из созданного файла
         print(f.read(n))
 
 if __name__ == "__main__":
-    print("\nСКРИПТ ЗАПУСКАЕТСЯ НАПРЯМУЮ")
+    print("\nСКРИПТ ЗАПУСКАЕТСЯ НАПРЯМУЮ\n")
     main()
 else:
     main()
